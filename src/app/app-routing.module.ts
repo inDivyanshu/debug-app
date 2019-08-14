dc4d04156a320b22c7566a27d535e5646248f715
Divyanshu Agarwal <divya.anshu30@gmail.com>
2019-08-14 11:56:40 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'homepage', loadChildren: './pages/homepage/homepage.module#HomepagePageModule' },
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { LottieSplashScreen } from '@ionic-native/lottie-splash-screen/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +12,29 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  flag:boolean=false;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router:Router,
+    public lottie:LottieSplashScreen
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.statusBar.show();
+//this if-loop is a template for the activities which are done to test wether the user should go to homepage or login screen
+      if(!this.flag){
+        this.router.navigate(['homepage'],{replaceUrl:true})
+   this.lottie.hide();      
+      } 
+      else{
+   this.lottie.hide();
+
+      }
     });
   }
 }
